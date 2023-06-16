0e812de2881da7e4883361b4b2744e89f833e494
razhterize <arza.mahendra.31@gmail.com>
2023-06-16 09:59:16 +0700

    test keyboard

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,19 @@
 #include "Arduino.h"
+#include "BleKeyboard.h"
 
-int prevNextCode;
-int store;
-int counter = 0;
+BleKeyboard keyboard;
 
-int dt = 5, clk = 21;
-
-int readRotary();
-
-IRAM_ATTR void isr()
-{
-  counter += readRotary();
-}
-
-void setup()
-{
+void setup(){
   Serial.begin(115200);
-  pinMode(dt, INPUT);
-  pinMode(clk, INPUT);
-  // attachInterrupt(dt, isr, RISING);
-  attachInterrupt(clk, isr, RISING);
+  keyboard.begin();
 }
 
-void loop()
-{
-}
-int readRotary()
-{
-  static int8_t encoder_table[16] = {0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0};
-  prevNextCode <<= 2;
-  if (digitalRead(dt))
-    prevNextCode |= 0x02;
-  if (digitalRead(clk))
-    prevNextCode |= 0x01;
-  prevNextCode &= 0x0f;
-
-  // If valid store as 16-bit data
-  if (encoder_table[prevNextCode])
-  {
-    store <<= 4;
-    store |= prevNextCode;
-    if ((store & 0xff) == 0x2b)
-    {
-      return -1;
-    }
-    if ((store & 0xff) == 0x17)
-    {
-      return 1;
-    }
+void loop(){
+  if (keyboard.isConnected()){
+    Serial.println("Sending Data...");
+    keyboard.print("Bitch");
+    delay(500);
+    keyboard.write(KEY_PAGE_DOWN);
   }
-  return 0;
+  delay(5000);
 }
