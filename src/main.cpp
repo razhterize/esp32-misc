c99dbe35f2115ae90b13f8bd5e8dd10bf75a862c
razhterize <arza.mahendra.31@gmail.com>
2023-06-24 16:10:23 +0700

    remove keyboard libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,2 @@
 #include "Arduino.h"
-#include "BleKeyboard.h"
-
-BleKeyboard keyboard;
-
-void setup(){
-  Serial.begin(115200);
-  keyboard.begin();
-}
-
-void loop(){
-  if (keyboard.isConnected()){
-    Serial.println("Sending Data...");
-    keyboard.print("Bitch");
-    delay(500);
-    keyboard.write(KEY_PAGE_DOWN);
-  }
-  delay(5000);
-}
+// Empty Template
